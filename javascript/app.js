c6d3b6c766a75ba418236a47e81b6c4d93141b8d
rusticpenguin <georgechios.music@gmail.com>
2018-08-30 19:42:46 -0600

    Can parse classes now

--- javascript/app.js
+++ javascript/app.js
@@ -4,27 +4,38 @@ function convertToJson(){
 }
 
 function parseHtml(data){
-    jsonParse = { component: []}
+    jsonParse = { component: [{}]}
+    let currentElement = 0;
     for(let i = 0; i < data.length; i++){
         if (data[i] === "<" && data[i + 1] !== "/"){
-   
-            jsonParse.component.push({element: ""})
+            //Create Element
+            jsonParse.component[currentElement].element = "";
             for(let j = 1; j < data.length; j++){
                 if (data[i + j] !== ">"){
-                    console.log("Hi")
-                    jsonParse.component[0].element += data[i + j]
+                    if ((data[j] === " ") && ((data[j + 1] + data[j + 2] + data[j + 3] + data[j + 4] + data[j + 5]) === "class")){
+                        //Create Class
+                        jsonParse.component[currentElement].class = "";
+                        for(let k = 8; k < data.length; k++){
+                            if (data[k + j] !== "\""){
+                                jsonParse.component[currentElement].class += data[j + k];
+                            } else {
+                                j += k;
+                                k = data.length
+                            }
+                        }
+                        console.log(j)
+                    } else {
+                        jsonParse.component[currentElement].element += data[i + j]
+                    }
                 } else {
-                    console.log("end")
                     i += j + 1;
-                    j = data.lenth;
+                    j = data.length;
                 }
             }
         } else if (data[i] === "<" && data[i + 1] === "/"){
-            console.log(jsonParse + " end")
+            console.log(jsonParse)
             return jsonParse
         }
-        console.log(i)
-        console.log(data[i])
     }
     console.log(jsonParse)
 }
