6dc825fd57c93d39039e35d57715df2a4c46a560
rusticpenguin <georgechios.music@gmail.com>
2018-08-30 19:11:57 -0600

    Can now parse the first element and assign it to a key

--- javascript/app.js
+++ javascript/app.js
@@ -4,5 +4,27 @@ function convertToJson(){
 }
 
 function parseHtml(data){
-    return data
+    jsonParse = { component: []}
+    for(let i = 0; i < data.length; i++){
+        if (data[i] === "<" && data[i + 1] !== "/"){
+   
+            jsonParse.component.push({element: ""})
+            for(let j = 1; j < data.length; j++){
+                if (data[i + j] !== ">"){
+                    console.log("Hi")
+                    jsonParse.component[0].element += data[i + j]
+                } else {
+                    console.log("end")
+                    i += j + 1;
+                    j = data.lenth;
+                }
+            }
+        } else if (data[i] === "<" && data[i + 1] === "/"){
+            console.log(jsonParse + " end")
+            return jsonParse
+        }
+        console.log(i)
+        console.log(data[i])
+    }
+    console.log(jsonParse)
 }
